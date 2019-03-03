839bc30268de08cc6e65bed1e09c073749517410
Gian Luca Rivera <gian.luca.99@hotmail.com>
2019-03-03 17:33:01 -0600

    Avances

--- app/src/main/java/com/example/gianlucariverabiagioni/lab7intento2/Data/Contact.kt
+++ app/src/main/java/com/example/gianlucariverabiagioni/lab7intento2/Data/Contact.kt
@@ -2,6 +2,7 @@ package com.example.gianlucariverabiagioni.lab7intento2.Data
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.sql.Blob
 
 //Data class con los atributos que posee un contacto
 @Entity(tableName = "contact_table")
@@ -10,6 +11,7 @@ data class Contact(
     var phone: String,
     var email: String,
     var priority: Int
+    //var image: Blob
 ){
     //Llave primaria de la tabla de DB
     @PrimaryKey(autoGenerate = true)
